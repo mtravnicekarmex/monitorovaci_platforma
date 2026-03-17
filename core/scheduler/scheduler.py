46545569114449dcd58681a8b3f8d20a06fa715e
tra <travnicek.michal@gmail.com>
2026-03-17 08:06:52 +0100

    před aplikací alertingu

--- core/scheduler/scheduler.py
+++ core/scheduler/scheduler.py
@@ -333,9 +333,9 @@ def locked_job(fn):
 # každých 15 minut
 @locked_job
 def quarter_hour_job():
-    # safe_call(vodomery_db_import)
-    # safe_call(score_new_measurements, model_version=1)
-    # safe_call(detect_events_from_scores, model_version=1)
+    safe_call(vodomery_db_import)
+    safe_call(score_new_measurements, model_version=1)
+    safe_call(detect_events_from_scores, model_version=1)
     # safe_call(meteo_sync)
     # safe_call(rebuild_profiles, 1)
     # safe_call(uloz_manometry_parquet)
